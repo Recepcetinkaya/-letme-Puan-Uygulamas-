5cf50d54595f0dadcc716d05710d15ce97d79d03
Recepcetinkaya <95712866+Recepcetinkaya@users.noreply.github.com>
2024-04-05 17:51:58 +0300

    merhaba

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Huzurlarinizda React Native with Expo!</Text>
       <StatusBar style="auto" />
-    </View>
-  );
+    </View>  );
 }
 
 const styles = StyleSheet.create({
